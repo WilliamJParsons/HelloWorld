b51b2dd46011433cc193f84f8f6a0021b513207e
root <root@WJPRHEL72.evszg3lf0eiehjbgy1chq1brub.bx.internal.cloudapp.net>
2018-06-05 22:56:39 +0000

    Added config to bring in the Azure CLI

--- setup.pp
+++ setup.pp
@@ -1,3 +1,18 @@
 package { 'git' :
   ensure => installed
   }
+
+yumrepo { 'azure-cli' :
+  gpgkey => 'https://packages.microsoft.com/keys/microsoft.asc',
+  ensure => present,
+  baseurl => 'https://packages.microsoft.com/yumrepos/azure-cli',
+  enabled => '1',
+  gpgcheck => '1',
+  }
+
+package { 'azure-cli' :
+  ensure => installed,
+  require => Yumrepo['azure-cli'],
+  }
+  
+
